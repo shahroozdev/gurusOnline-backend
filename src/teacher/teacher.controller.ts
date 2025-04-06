67faf06bef98e56921ab238809ec596e255e6c19
shahroozdev <shahroozdeveloper@gmail.com>
2025-04-06 16:30:24 +0500

    adding teacher routes

--- src/teacher/teacher.controller.ts
+++ src/teacher/teacher.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Public } from 'src/common/decorators/public.decorator';
+import { apiWrapper } from 'src/common/globalErrorHandlerClass';
+import { TeacherService } from './teacher.service';
 
 @Controller('teacher')
-export class TeacherController {}
+export class TeacherController {
+    constructor(private TeacherService: TeacherService) {}
+          @Public()
+          @Post('/registerLead')
+          @HttpCode(200)
+        //   @ApiCustomResponse('signUp')
+        registerLead(
+            @Body() dto: any,
+          ): Promise<{ user?: any; message?: string; status: number }> {
+            return apiWrapper(() => this.TeacherService.registerLead(dto));
+          }
+    
+          @Post('/register')
+          @HttpCode(200)
+          registerTeacher(
+            @Body() dto: any,
+          ): Promise<{ user?: any; message?: string; status: number }> {
+            return apiWrapper(() => this.TeacherService.registerTeacher(dto));
+          }
+}
--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { response } from 'src/common/types';
+import { MailService } from 'src/mail/mail.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class TeacherService {}
+export class TeacherService {
+  constructor(
+    private Prisma: PrismaService,
+    private MailService: MailService,
+  ) {}
+
+  async registerTeacher(dto: any): Promise<response> {
+    return;
+  }
+  async registerLead(dto: any): Promise<response> {
+    return { status: 200, message: 'hello' };
+  }
+}
