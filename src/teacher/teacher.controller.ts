bfa083705b2cac337fbe0339305ccac8a0141834
shahroozdev <shahroozdeveloper@gmail.com>
2025-04-06 16:44:13 +0500

    teacher get apis endpoints

--- src/teacher/teacher.controller.ts
+++ src/teacher/teacher.controller.ts
@@ -1,26 +1,38 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { Public } from 'src/common/decorators/public.decorator';
 import { apiWrapper } from 'src/common/globalErrorHandlerClass';
 import { TeacherService } from './teacher.service';
 
 @Controller('teacher')
 export class TeacherController {
-    constructor(private TeacherService: TeacherService) {}
-          @Public()
-          @Post('/registerLead')
-          @HttpCode(200)
-        //   @ApiCustomResponse('signUp')
-        registerLead(
-            @Body() dto: any,
-          ): Promise<{ user?: any; message?: string; status: number }> {
-            return apiWrapper(() => this.TeacherService.registerLead(dto));
-          }
-    
-          @Post('/register')
-          @HttpCode(200)
-          registerTeacher(
-            @Body() dto: any,
-          ): Promise<{ user?: any; message?: string; status: number }> {
-            return apiWrapper(() => this.TeacherService.registerTeacher(dto));
-          }
+  constructor(private TeacherService: TeacherService) {}
+  @Public()
+  @Post('/registerLead')
+  @HttpCode(200)
+  //   @ApiCustomResponse('signUp')
+  registerLead(
+    @Body() dto: any,
+  ): Promise<{ user?: any; message?: string; status: number }> {
+    return apiWrapper(() => this.TeacherService.registerLead(dto));
+  }
+
+  @Post('/register')
+  @HttpCode(200)
+  registerTeacher(
+    @Body() dto: any,
+  ): Promise<{ user?: any; message?: string; status: number }> {
+    return apiWrapper(() => this.TeacherService.registerTeacher(dto));
+  }
+  @Get('/teachers')
+  @HttpCode(200)
+  allTeachers(): Promise<{ teachers?: any; message?: string; status: number }> {
+    return apiWrapper(() => this.TeacherService.allTeachers());
+  }
+  @Get('/teacher/:id')
+  @HttpCode(200)
+  teacherById(
+    @Param('id') id: string,
+  ): Promise<{ teacher?: any; message?: string; status: number }> {
+    return apiWrapper(() => this.TeacherService.teacherById(Number(id)));
+  }
 }
--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -16,4 +16,18 @@ export class TeacherService {
   async registerLead(dto: any): Promise<response> {
     return { status: 200, message: 'hello' };
   }
+  async allTeachers():Promise<response & {teachers?:any}>{
+    const teachers = await this.Prisma.user.findMany({where:{roleId:2}});
+    if(!teachers){
+        return {status:404, message:'Teachers not found'}
+    }
+    return {status:200, message:'All teachers List', teachers}
+}
+async teacherById(id:number):Promise<response & {teacher?:any}>{
+    const teacher = await this.Prisma.user.findUnique({where:{id}});
+    if(!teacher){
+        return {status:404, message:'Teacher not found'}
+    }
+    return {status:200, message:'Teacher fetch successfully', teacher}
+}
 }
